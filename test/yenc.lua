056b04855b59003d29477a74c73310b674732063
qruf <qruf@b23.be>
2016-11-15 00:58:00 +0000

    update tests

--- test/yenc.lua
+++ test/yenc.lua
@@ -1,30 +1,33 @@
 local assert   = require "test.assert"
 local yenc     = require "yenc"
 
--- yenc.crc32
-local crc = yenc.crc32("old woman killed by little glass planet", 39)
-assert.equal(crc, 0x57F9E257)
-
--- yenc.crc32_combine
-local crc1, crc2 = yenc.crc32("old woman killed by ", 20), yenc.crc32("little glass planet", 19)
-local crcc = yenc.crc32_combine(crc1, crc2, 19)
-assert.equal(crc, 0x57F9E257)
-
--- yenc.encode
 local fd_src = io.open("test/test.bin")
 local src = fd_src:read("*a")
-local fd_ync = io.open("test/test.bin.ync")
-local ync = fd_ync:read("*a")
+local fd_ref = io.open("test/test.bin.ync")
+local ref = fd_ref:read("*a")
 fd_src:close()
-fd_ync:close()
-local crc_src = yenc.crc32(src, 16384)
-assert.equal(crc_src, 0x212D3568)
+fd_ref:close()
+
+-- yenc.crc32
+local crc_ref = yenc.crc32(src, 16384)
+assert.equal(crc_ref, 0x212D3568)
+
+-- yenc.crc32_combine
+local crc_cmb = yenc.crc32_combine(crc_ref, crc_ref, 16384)
+assert.equal(crc_cmb, 0xD612F035)
+
+-- yenc.encode
 local enc, crc_enc = yenc.encode(src, 16384, 128, 0)
-assert(enc == ync)
-assert.equal(crc_src, crc_enc)
+assert(enc == ref)
+assert.equal(crc_enc, crc_ref)
+
+local enc, crc_enc, crc_cmb = yenc.encode(src, 16384, 128, crc_enc)
+assert(enc == ref)
+assert.equal(crc_enc, crc_ref)
+assert.equal(crc_cmb, 0xD612F035)
 
 -- yenc.decode
 local dec, crc_dec = yenc.decode(enc, 16384, 0)
 assert(src == dec)
-assert.equal(crc_src, crc_dec)
+assert.equal(crc_ref, crc_dec)
 
